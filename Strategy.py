5779d7f5f39d15d24fa6cb359331c843b499fd02
Raimondo <SoyerTek@gmail.com>
2022-10-18 14:44:48 +0200

    Added SimpleStrategyV1-V2-V3-V4

--- Strategy.py
+++ Strategy.py
@@ -1,10 +1,14 @@
 from abc import abstractmethod
+from math import floor
 from random import randrange
 
 
 class Strategy :
     @abstractmethod
     def move(self, board): pass
+    
+    def __str__(self) -> str:
+        return self.__class__.__name__
 
 class MiniMaxStrategy(Strategy):
     """
@@ -28,9 +32,117 @@ class PrbabilityStrategy(Strategy):
     def move(self, board):
         return super().move()
     
-class SimpleStrategy(Strategy):
+class SimpleStrategyV4(Strategy):
     """
-    Choses a move based on very simple rules
+    Choses a move based on very simple rules:
+    uses the lowest card of the same seed that will win
+    or the lowest briscola
+    or the card with the lowest value
+    """
+    def move(self, board):
+        hand = board.currentPlayer.hand
+        if len(board.table)==0 : #if it's the first player chooses random card
+            x = sorted(hand, key=lambda x: x.number*10 if x.seed==board.briscola.seed else x.number, reverse=True)
+            return hand.index(x[len(x)-1])
+        else : #otherwise try to win with the lowest option
+            firstCard = list(board.table.values())[0]
+            lowestBriscola=-1
+            lowestFirst=-1#the card with wich the turn started
+            lowestGeneral=-1
+            i=0
+            for card in hand:
+                lowestGeneral = i if lowestGeneral==-1 or card.compare(hand[lowestGeneral]) < 0 else lowestGeneral
+                if card.seed == firstCard.seed :
+                    if card.compare(firstCard) > 0 :
+                        if lowestFirst == -1 or card.compare(hand[lowestFirst]) < 0:
+                            lowestFirst = i
+                if card.seed == board.briscola.seed :
+                    lowestBriscola = i if lowestBriscola==-1 or card.compare(hand[lowestBriscola]) < 0 else lowestBriscola
+                i+=1
+
+            if lowestFirst != -1 : 
+                return lowestFirst
+            elif lowestBriscola != -1 and firstCard.seed != board.briscola.seed:
+                return lowestBriscola
+            else: 
+                return lowestGeneral
+    
+class SimpleStrategyV3(Strategy):
+    """
+    Choses a move based on very simple rules:
+    uses the lowest card of the same seed that will win
+    or the lowest briscola
+    or the card with the lowest value
+    """
+    def move(self, board):
+        hand = board.currentPlayer.hand
+        if len(board.table)==0 : #if it's the first player chooses random card
+            x = sorted(hand, key=lambda x: x.number*10 if x.seed==board.briscola.seed else x.number, reverse=True)
+            return hand.index(x[0])
+        else : #otherwise try to win with the lowest option
+            firstCard = list(board.table.values())[0]
+            lowestBriscola=-1
+            lowestFirst=-1#the card with wich the turn started
+            lowestGeneral=-1
+            i=0
+            for card in hand:
+                lowestGeneral = i if lowestGeneral==-1 or card.compare(hand[lowestGeneral]) < 0 else lowestGeneral
+                if card.seed == firstCard.seed :
+                    if card.compare(firstCard) > 0 :
+                        if lowestFirst == -1 or card.compare(hand[lowestFirst]) < 0:
+                            lowestFirst = i
+                if card.seed == board.briscola.seed :
+                    lowestBriscola = i if lowestBriscola==-1 or card.compare(hand[lowestBriscola]) < 0 else lowestBriscola
+                i+=1
+
+            if lowestFirst != -1 : 
+                return lowestFirst
+            elif lowestBriscola != -1 and firstCard.seed != board.briscola.seed:
+                return lowestBriscola
+            else: 
+                return lowestGeneral
+    
+class SimpleStrategyV2(Strategy):
+    """
+    Choses a move based on very simple rules:
+    uses the lowest card of the same seed that will win
+    or the lowest briscola
+    or the card with the lowest value
+    """
+    def move(self, board):
+        hand = board.currentPlayer.hand
+        if len(board.table)==0 : #if it's the first player chooses random card
+            x = sorted(hand, key=lambda x: x.number*10 if x.seed==board.briscola.seed else x.number, reverse=True)
+            return hand.index(x[0 if len(x)==1 else 1])
+        else : #otherwise try to win with the lowest option
+            firstCard = list(board.table.values())[0]
+            lowestBriscola=-1
+            lowestFirst=-1#the card with wich the turn started
+            lowestGeneral=-1
+            i=0
+            for card in hand:
+                lowestGeneral = i if lowestGeneral==-1 or card.compare(hand[lowestGeneral]) < 0 else lowestGeneral
+                if card.seed == firstCard.seed :
+                    if card.compare(firstCard) > 0 :
+                        if lowestFirst == -1 or card.compare(hand[lowestFirst]) < 0:
+                            lowestFirst = i
+                if card.seed == board.briscola.seed :
+                    lowestBriscola = i if lowestBriscola==-1 or card.compare(hand[lowestBriscola]) < 0 else lowestBriscola
+                i+=1
+
+            if lowestFirst != -1 : 
+                return lowestFirst
+            elif lowestBriscola != -1 and firstCard.seed != board.briscola.seed:
+                return lowestBriscola
+            else: 
+                return lowestGeneral
+    
+class SimpleStrategyV1(Strategy):
+    """
+    Choses a move based on very simple rules:
+    uses the lowest card of the same seed that will win
+    or the lowest briscola
+    or the card with the lowest value
     """
     def move(self, board):
         if len(board.table)==0 : #if it's the first player chooses random card
@@ -44,17 +156,17 @@ class SimpleStrategy(Strategy):
             i=0
             for card in hand:
                 lowestGeneral = i if lowestGeneral==-1 or card.compare(hand[lowestGeneral]) < 0 else lowestGeneral
-                if card.seed == board._briscola.seed :
-                    lowestBriscola = i if lowestBriscola==-1 or card.compare(hand[lowestBriscola]) < 0 else lowestBriscola
                 if card.seed == firstCard.seed :
                     if card.compare(firstCard) > 0 :
                         if lowestFirst == -1 or card.compare(hand[lowestFirst]) < 0:
                             lowestFirst = i
+                if card.seed == board._briscola.seed :
+                    lowestBriscola = i if lowestBriscola==-1 or card.compare(hand[lowestBriscola]) < 0 else lowestBriscola
                 i+=1
 
             if lowestFirst != -1 : 
                 return lowestFirst
-            elif lowestBriscola != -1 :
+            elif lowestBriscola != -1 and firstCard.seed != board.briscola.seed:
                 return lowestBriscola
             else: 
                 return lowestGeneral
